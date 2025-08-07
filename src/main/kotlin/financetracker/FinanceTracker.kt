469f5fef60608f5b2a2d623956f25589dd569340
arshgorithm <s.a.sajid@outlook.com>
2025-08-07 19:37:39 +0600

    finance completed

--- src/main/kotlin/financetracker/FinanceTracker.kt
+++ src/main/kotlin/financetracker/FinanceTracker.kt
@@ -76,8 +76,8 @@ class FinanceTracker {
         if (data.accounts.isEmpty()) println("No Accounts are created.")
         else{
             println("Available Accounts:")
-            data.accounts.forEach { account ->
-                println(account.name)
+            for((i,account) in data.accounts.withIndex()){
+                println("${i+1}. ${account.name}")
             }
 
 
@@ -90,6 +90,11 @@ class FinanceTracker {
         if (existingAccount != null) {
             existingAccount.transactions.add(transaction)
         } else println("Account '${account.name}' doesn't exist...")
+    }
+    fun showAllTransaction(){
+        data.transactions.forEach {
+            println("${it.id} | ${it.amount.toCurrency()} | ${it.date} \n ${it.type} | ${it.category} ${it.description}")
+        }
     }
     fun setBudget(category: Category,limit: Double,period: BudgetPeriod){
         val updateBudget = Budget(category,limit,period)
@@ -172,60 +177,79 @@ class FinanceTracker {
 fun Double.toCurrency(): String = String.format("৳%.2f",this)
 
 fun main() {
- val manager = FileManager()
-    val tracker = FinanceTracker()
-    println("Load saved account: y/n ")
-    val input = readlnOrNull()
-    if (input != null){
-        when(input){
-            "y" -> manager.loadFinanceData("finance-tracker.json")
-            "n" -> println("Create new account: ")
-
-        }
-    }
-}
-
-/*
-val tracker = FinanceTracker()
     val manager = FileManager()
-    println("Enter Account Name:")
-    val accName = readln()
+    val fileName = "finance-tracker.json"
+    val tracker = FinanceTracker()
+    var account: Account?
+    println("${"-".repeat(10)} Welcome to Finance Tracker ${"_".repeat(10)}")
+    val loadedData = manager.loadFinanceData(fileName)
+    if (loadedData?.accounts.isNullOrEmpty()) {
+        println("Yo Gorib \n You don't have any account ")
+        println("Create an account(y/n)")
+        val choice = readlnOrNull()
+        if (choice != null) {
+            when (choice.lowercase()) {
+                "y" -> {
+                    println("Enter account name: ")
+                    val accName = readlnOrNull()
+                    println("Enter initial amount:")
+                    val initialBalance = readln().toDoubleOrNull() ?: 0.0
+                    if (accName != null) {
+                        account = Account(accName, initialBalance)
+                        tracker.addAccount(account)
+                    } else println("Invalid input.Try again.")
+                }
 
-    println("Enter Initial Balance:")
-    val initialBalance = readln().toDoubleOrNull() ?: 0.0
+                "n" -> return
 
-    val account = Account(name = accName, initialBalance = initialBalance)
-    tracker.addAccount(account)
-
-    println("${"-".repeat(10)} Welcome to Finance Tracker ${"-".repeat(10)}")
+            }
+        } else println("Invalid input.")
+    } else {
+        tracker.data = loadedData
+    }
     while (true) {
+        println("Select a default account:")
+        tracker.printAllAccount()
+        val input = readlnOrNull()?.toIntOrNull()
+        if (input == null || input !in 1..tracker.data.accounts.size) {
+            println("Invalid account selection.")
+            continue
+        }
+        val defaultAccount = tracker.data.accounts[input - 1]
         println(
             """
-
-            ==== MENU ====
-            1. Add Transaction
-            2. View Account Summary
-            3. Save Account
-            4. Load Account
-            5. Set Budget
-            6. Generate Monthly Report
+                ==== MENU ====
+            1. Account Summary
+            2. Add Transaction
+            3. Set Budget
+            4. Generate Report
+            5. Print All Budget
+            6. Print All Accounts 
+            7. Save Data
+            8. Load Data
+            9. All Transactions
             0. Exit
-            """.trimIndent()
+            
+        """.trimIndent()
         )
-        print("Choose an option: ")
+        println("Choose an Option")
         when (readln()) {
             "1" -> {
+                tracker.printAccountSummary(defaultAccount.name)
+            }
+
+            "2" -> {
                 println("➕ Add Transaction")
-                print("Amount: ")
+                println("Amount:")
                 val amount = readln().toDouble()
-                print("Description: ")
-                val desc = readln()
-                println("Type: 1) INCOME  2) EXPENSE")
+                println("Description:")
+                val desc = readlnOrNull() ?: ""
+                println("Transaction type: 1.INCOME 2.EXPENSE ")
                 val type = when (readln()) {
                     "1" -> TransactionType.INCOME
                     else -> TransactionType.EXPENSE
                 }
-                println("Category: 1) FOOD 2) SALARY 3) TRAVEL 4) ENTERTAINMENT 5) OTHERS")
+                println("Select Category: 1) FOOD 2) SALARY 3) TRAVEL 4) ENTERTAINMENT 5) OTHERS")
                 val category = when (readln()) {
                     "1" -> Category.FOOD
                     "2" -> Category.SALARY
@@ -234,67 +258,114 @@ val tracker = FinanceTracker()
                     else -> Category.OTHERS
                 }
                 val transaction = Transaction(amount = amount, description = desc, type = type, category = category)
-                tracker.addTransaction(transaction, account)
+                println("Choose an account: ")
+                tracker.printAllAccount()
+                tracker.addTransaction(transaction, defaultAccount)
                 println("✅ Transaction added.")
             }
 
-            "2" -> tracker.printAccountSummary(account.name)
             "3" -> {
-                manager.saveFinanceData(tracker.data, "finance-tracker.json")
-                println("✅ Account data saved to file.")
-            }
-
-            "4" -> {
-                val loaded = manager.loadFinanceData("finance-tracker.json")
-                if (loaded != null) {
-                    tracker.data = loaded
-                    println("✅ Data loaded.")
-                } else println("❌ No saved data found.")
-            }
-
-            "5" -> {
-                println(" Set Budget ")
-
-                println("Choose category (1-5): 1) FOOD 2) SALARY 3) TRAVEL 4) ENTERTAINMENT 5) OTHERS")
-                val cat = when (readln()) {
+                println("Select Category: 1) FOOD 2) SALARY 3) TRAVEL 4) ENTERTAINMENT 5) OTHERS")
+                val category = when (readln()) {
                     "1" -> Category.FOOD
                     "2" -> Category.SALARY
                     "3" -> Category.TRAVEL
                     "4" -> Category.ENTERTAINMENT
                     else -> Category.OTHERS
                 }
-                print("Enter budget limit: ")
-                val limit = readln().toDouble()
-                println("Period: 1) WEEKLY 2) MONTHLY 3) YEARLY")
+                println("Set a limit:")
+                val limit = readln().toDoubleOrNull() ?: 0.0
+                println("Select Period:1.WEEKLY 2.MONTHLY 3.YEARLY")
                 val period = when (readln()) {
                     "1" -> BudgetPeriod.WEEKLY
                     "2" -> BudgetPeriod.MONTHLY
                     else -> BudgetPeriod.YEARLY
                 }
-                tracker.setBudget(cat, limit, period)
+                tracker.setBudget(category, limit, period)
                 println("✅ Budget set.")
+            }
 
+            "4" -> {
+                println("Choose a report: 1.General Report 2.Category Report 3.Period Report")
+                val reportChoice = readln()
+                when (reportChoice) {
+                    "1" -> {
+                        println("📆 General Report")
+                        val gReport = tracker.generateReport()
+                        println("Total Income:${gReport.totalIncome.toCurrency()} ")
+                        println("Total Expense:${gReport.totalExpense.toCurrency()} ")
+                        println("Net Balance:${gReport.net.toCurrency()} ")
+                        println("Total Transactions:${gReport.transactionCount}")
+                    }
+
+                    "2" -> {
+                        println("Select Category: 1) FOOD 2) SALARY 3) TRAVEL 4) ENTERTAINMENT 5) OTHERS")
+                        val reportCategory = when (readln()) {
+                            "1" -> Category.FOOD
+                            "2" -> Category.SALARY
+                            "3" -> Category.TRAVEL
+                            "4" -> Category.ENTERTAINMENT
+                            else -> Category.OTHERS
+                        }
+                        val cReport = tracker.generateCategoryReport(reportCategory)
+                        println("${"-".repeat(5)}Category Report(${cReport.category})${"-".repeat(5)}")
+                        println("Total Income:${cReport.totalIncome.toCurrency()} ")
+                        println("Total Expense:${cReport.totalExpense.toCurrency()} ")
+                        println("Net Balance:${cReport.net.toCurrency()} ")
+                    }
+
+                    "3" -> {
+                        println("Select Period:1.WEEKLY 2.MONTHLY 3.YEARLY")
+                        val period = when (readln()) {
+                            "1" -> BudgetPeriod.WEEKLY
+                            "2" -> BudgetPeriod.MONTHLY
+                            else -> BudgetPeriod.YEARLY
+                        }
+                        val pReport = tracker.generatePeriodReport(period)
+                        println("${"-".repeat(5)} ${pReport.period} Report ${"-".repeat(5)}")
+                        println("From ${pReport.from} to ${pReport.to}")
+                        println("Total Income:${pReport.totalIncome.toCurrency()} ")
+                        println("Total Expense:${pReport.totalExpense.toCurrency()} ")
+                        println("Net Balance:${pReport.net.toCurrency()} ")
+
+                    }
+                }
 
             }
 
-            "6" -> {
-                val report = tracker.generatePeriodReport(BudgetPeriod.MONTHLY)
-                println("📆 Monthly Report")
-                println("From ${report.from} to ${report.to}")
-                println("Income: ${report.totalIncome.toCurrency()}")
-                println("Expense: ${report.totalExpense.toCurrency()}")
-                println("Net: ${(report.totalIncome - report.totalExpense).toCurrency()}")
+            "5" -> {
+                println("All Budgets:")
+                tracker.printAllBudget()
+            }
 
+            "6" -> {
+                tracker.printAllAccount()
+            }
+
+            "7" -> {
+                manager.saveFinanceData(tracker.data, fileName)
+                println("✅ Account data saved to file.")
+            }
+
+            "8" -> {
+                val loaded = manager.loadFinanceData(fileName)
+                if (loaded != null) {
+                    tracker.data = loaded
+                    println("✅ Data loaded.")
+                } else println("❌ No saved data found.")
+
+            }
+            "9" -> {
+                tracker.showAllTransaction()
             }
 
             "0" -> {
                 println("👋 Exiting. Goodbye!")
                 break
+
             }
-
-            else -> println("❗ Invalid option.")
-
-
         }
+
+
     }
- */
+}
