f8cce8e6e409f0fd98df7aad335b1b0546e1e0b5
arshgorithm <s.a.sajid@outlook.com>
2025-10-24 23:47:50 +0600

    a

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,16 +17,17 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    testImplementation(kotlin("test"))
 }
 application {
 
 }
-
-
-
 tasks.test {
     useJUnitPlatform()
 }
 kotlin {
     jvmToolchain(21)
 }
+
+
+
